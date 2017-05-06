c2d708538e9a6f71136f110aa682bc9454ce2379
lkayda <ankayda@gmail.com>
2017-05-06 22:09:10 +0300

    initial commit

---
+++ src/Launcher.java
@@ -0,0 +1,11 @@
+/**
+ * Created by 248 on 06.05.2017.
+ * Main class that start the app work
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
